9efb083865d801bb3b265c8e2a542948a4b16bba
ayanchax <42091907+ayanchax@users.noreply.github.com>
2022-01-13 02:51:50 +0530

    commit

--- components/Authorized/Sessions/Forms/Steps/Session/Schedule.js
+++ components/Authorized/Sessions/Forms/Steps/Session/Schedule.js
@@ -2,9 +2,10 @@ import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import { MuiPickersUtilsProvider } from "@material-ui/pickers";
-import DateFnsUtils from "@date-io/date-fns"; // choose your lib
+import DateFnsUtils from "@date-io/date-fns";
 import { DatePicker } from "@material-ui/pickers";
 import InputLabel from "@mui/material/InputLabel";
+import TextField from "@mui/material/TextField";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
@@ -14,16 +15,50 @@ import { formatTime, getTimezone } from "../../../../../../utils/utility";
 import Checkbox from "@mui/material/Checkbox";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Spacer from "../../../../../shared/Spacer";
-
+import { SCHEDULE } from "../../../../../../constants/schedule";
+import FormHelperText from "@mui/material/FormHelperText";
+import ScheduleModuleCSS from "../../../../../../styles/Schedule.module.css";
+import parse from "html-react-parser";
 function Schedule(props) {
   const [timezone, setTimeZone] = useState(getTimezone());
+  const [schedule, setSchedule] = useState(SCHEDULE);
+  const [selectedStartDate, setSelectedStartDate] = React.useState(new Date());
+  const [repeatObject, setRepeatObject] = useState(null);
+  const [repeatChecked, setRepeatChecked] = useState(false);
+  const [repeatValue, setRepeatValue] = useState("");
+  const [repeatEvery, setRepeatEvery] = useState(0);
+  const [occurenceCount, setOccurenceCount] = useState(0);
+  const handleRepeatCheckChange = (event) => {
+    setRepeatChecked(event.target.checked);
+  };
+  const handleOccurenceCountChange = (event) => {
+    console.log(event);
+    setOccurenceCount(event.target.value);
+  };
+  const handleRepeatsChange = (event) => {
+    const repeatObject = schedule.find(
+      (_sch) => _sch.value === event.target.value
+    );
+    setOccurenceCount(repeatObject.endAfter.occurences);
+    setRepeatObject(repeatObject);
+    setRepeatValue(event.target.value);
+  };
+  const handleRepeatEveryChange = (event) => {
+    setRepeatEvery(event.target.value);
+  };
   const label = {
-    inputProps: { "aria-label": "Repeat checkbox", id: "repeat-label" },
+    inputProps: {
+      "aria-label": "controlled repeat checkbox",
+      id: "repeat-label",
+    },
   };
   const changedDate = () => {
     console.log();
   };
-  const [selectedDate, handleDateChange] = React.useState(new Date());
+  const handleStartDateChange = (event) => {
+    console.log(event);
+    //setSelectedStartDate(new Date(event.target.value))
+  };
   return (
     <div className={`p-2`}>
       <Box sx={{ width: "100%" }}>
@@ -41,9 +76,9 @@ function Schedule(props) {
                   variant="static"
                   disablePast
                   openTo="date"
-                  value={selectedDate}
+                  value={selectedStartDate}
                   onChangeCapture={() => changedDate()}
-                  onChange={handleDateChange}
+                  onChange={handleStartDateChange}
                 />
               </FormControl>
             </MuiPickersUtilsProvider>
@@ -139,11 +174,161 @@ function Schedule(props) {
                 <Spacer />
                 <FormControlLabel
                   className="text-sm ml-0.5 text-gray-700"
-                  control={<Checkbox {...label} />}
+                  disabled={false}
+                  control={
+                    <Checkbox
+                      onChange={handleRepeatCheckChange}
+                      checked={repeatChecked}
+                      size="small"
+                      {...label}
+                    />
+                  }
                   label="Repeat"
                   labelPlacement="end"
                 />
               </div>
+
+              {repeatChecked && (
+                <div className="schedule__repeat__scheduler__wrapper mt-2">
+                  <FormControl
+                    fullWidth={true}
+                    variant="outlined"
+                    sx={{ marginBottom: 1 }}
+                  >
+                    <InputLabel id="repeats-label">Repeats</InputLabel>
+                    <Select
+                      className={`${
+                        repeatValue !== ""
+                          ? ScheduleModuleCSS.schedule__select
+                          : ScheduleModuleCSS.schedule__select
+                      }`}
+                      labelId="repeats-label"
+                      id="repeats"
+                      value={repeatValue}
+                      label="Repeats"
+                      onChange={handleRepeatsChange}
+                    >
+                      {schedule
+                        ?.filter((schedule) => schedule.display)
+                        .map((schedule) => (
+                          <MenuItem
+                            className="block p-3"
+                            key={schedule.id}
+                            value={schedule.value}
+                          >
+                            {schedule.displayValue}
+                          </MenuItem>
+                        ))}
+                    </Select>
+                  </FormControl>
+
+                  {repeatValue !== "" && (
+                    <>
+                      <FormControl
+                        fullWidth={true}
+                        variant="outlined"
+                        className={` mt-4`}
+                        sx={{ marginBottom: 1, marginTop: 3 }}
+                      >
+                        <InputLabel id="repeat-every-label">
+                          Repeat every
+                        </InputLabel>
+                        <Select
+                          className={`${
+                            repeatEvery > 0
+                              ? ScheduleModuleCSS.schedule__select
+                              : ""
+                          }`}
+                          labelId="repeat-every-label"
+                          id="repeat-every"
+                          value={repeatEvery}
+                          label="Repeat every"
+                          onChange={handleRepeatEveryChange}
+                          aria-describedby="repeat-every-label-helper-text"
+                        >
+                          {repeatObject.repeatsEvery.range?.map((obj) => (
+                            <MenuItem
+                              className="block p-3"
+                              key={obj}
+                              value={obj}
+                            >
+                              {obj}
+                            </MenuItem>
+                          ))}
+                        </Select>
+                        <FormHelperText
+                          className={`${ScheduleModuleCSS.schedule__form__helper__text}`}
+                          id="repeat-every-label-helper-text"
+                        >
+                          {parse(repeatObject.repeatsEvery.label)}
+                        </FormHelperText>
+                      </FormControl>
+                      <FormControl
+                        fullWidth={true}
+                        variant="outlined"
+                        sx={{ marginBottom: 1, marginTop: 1 }}
+                      >
+                        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                          <DatePicker
+                            disableToolbar
+                            variant="inline"
+                            label="Starts on"
+                            value={selectedStartDate}
+                            disabled
+                            disabledPast
+                          />
+                        </MuiPickersUtilsProvider>
+                      </FormControl>
+
+                      <div className="flex flex-col mt-2">
+                        <h4 className="text-gray-500 text-sm font-semibold">
+                          Ends
+                        </h4>
+
+                        <div
+                          className={`flex flex-col ${ScheduleModuleCSS.schedule__endsOnWrapper}`}
+                        >
+                          <div className="flex gap-2">
+                            <div className=" p-1 text-gray-500 text-xs font-semibold">
+                              After
+                            </div>
+                            <FormControl
+                              variant="filled"
+                              sx={{ marginBottom: 1, marginTop: 1, padding: 2 }}
+                            >
+                              <TextField
+                                onChange={handleOccurenceCountChange}
+                                value={occurenceCount}
+                                id="occurences"
+                                helperText="occurences"
+                                type="number"
+                                className={`${ScheduleModuleCSS.schedule__occurence__select}`}
+                              />
+                            </FormControl>
+                          </div>
+                          <div className="flex gap-2">
+                            <div className=" p-1 text-gray-500 text-xs font-semibold">
+                              On
+                            </div>
+                            <FormControl
+                              variant="filled"
+                              sx={{ marginBottom: 1, marginTop: 1 }}
+                            >
+                              <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                                <DatePicker
+                                  disableToolbar
+                                  variant="inline"
+                                  disabledPast
+                                />
+                              </MuiPickersUtilsProvider>
+                            </FormControl>
+                          </div>
+                        </div>
+                      </div>
+                    </>
+                  )}
+                </div>
+              )}
             </div>
           </Grid>
         </Grid>
---
+++ constants/schedule.js
@@ -0,0 +1,124 @@
+import { HTMLUnderlineByCharacterIndex } from "../utils/utility";
+import { DAYS_OF_WEEK } from "./calendar";
+
+export const SCHEDULE = [
+  {
+    id: 1,
+    value: "Daily",
+    displayValue: "Daily",
+    endAfter: {
+      occurences: 5,
+    },
+    weeklyRepeatBy: [],
+    repeatsEvery: {
+      label: HTMLUnderlineByCharacterIndex("days"),
+      range: [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+        21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+      ],
+    },
+    display: true,
+  },
+  {
+    id: 2,
+    value: "EveryWeekDay",
+    displayValue: "Every weekday (Monday to Friday)",
+    display: true,
+    weeklyRepeatBy: [],
+    repeatsEvery: {
+      label: HTMLUnderlineByCharacterIndex("weeks"),
+      range: [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+        21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+      ],
+    },
+    endAfter: {
+      occurences: 35,
+    },
+  },
+  {
+    id: 3,
+    value: "EveryMonWedFriday",
+    displayValue: "Every Monday, Wednesday, and Friday",
+    display: true,
+    weeklyRepeatBy: [],
+    repeatsEvery: {
+      label: HTMLUnderlineByCharacterIndex("weeks"),
+      range: [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+        21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+      ],
+    },
+    endAfter: {
+      occurences: 35,
+    },
+  },
+  {
+    id: 4,
+    value: "EveryTueThu",
+    displayValue: "Every Tuesday and Thursday",
+    display: true,
+    weeklyRepeatBy: [],
+    repeatsEvery: {
+      label: HTMLUnderlineByCharacterIndex("weeks"),
+      range: [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+        21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+      ],
+    },
+    endAfter: {
+      occurences: 35,
+    },
+  },
+  {
+    id: 5,
+    value: "Weekly",
+    displayValue: "Weekly",
+    display: true,
+    weeklyRepeatBy: DAYS_OF_WEEK,
+    repeatsEvery: {
+      label: HTMLUnderlineByCharacterIndex("weeks"),
+      range: [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+        21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+      ],
+    },
+    endAfter: {
+      occurences: 35,
+    },
+  },
+  {
+    id: 6,
+    value: "Monthly",
+    displayValue: "Monthly",
+    display: false,
+    weeklyRepeatBy: [],
+    repeatsEvery: {
+      label: "months",
+      range: [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+        21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+      ],
+    },
+    endAfter: {
+      occurences: 5,
+    },
+  },
+  {
+    id: 7,
+    value: "Yearly",
+    displayValue: "Yearly",
+    display: false,
+    weeklyRepeatBy: [],
+    repeatsEvery: {
+      label: "years",
+      range: [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+        21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+      ],
+    },
+    endAfter: {
+      occurences: 5,
+    },
+  },
+];
