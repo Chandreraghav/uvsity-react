bdd7a311400e5a741257e941fd24b075fd647804
ayanchax <42091907+ayanchax@users.noreply.github.com>
2022-05-29 02:54:43 +0530

    commit

--- components/shared/ReadMore.js
+++ components/shared/ReadMore.js
@@ -1,20 +1,32 @@
 import { Typography } from "@mui/material";
 import React, { useState } from "react";
 import { READ_LESS, READ_MORE } from "../../constants/constants";
-const ReadMore = ({ children }) => {
+const ReadMore = ({ children, coded }) => {
   const text = children;
   const [isReadMore, setIsReadMore] = useState(true);
   const toggleReadMore = () => {
     setIsReadMore(!isReadMore);
   };
   return (
-    <Typography className="text" variant="body2">
-       {isReadMore ? text.slice(0, 150) : text}
-      <span onClick={toggleReadMore} className="read-or-hide">
-        {isReadMore ? READ_MORE : READ_LESS}
-      </span>
-    </Typography>
-    
+    <>
+      <Typography
+        className={coded ? "code" : "text"}
+        variant={coded ? "caption" : "inherit"}
+      >
+        {isReadMore ? text.slice(0, 150) : text}
+      </Typography>
+      {coded ? (
+        <>
+          <div onClick={toggleReadMore} className="read-or-hide">
+            {isReadMore ? READ_MORE : READ_LESS}
+          </div>
+        </>
+      ) : (
+        <span onClick={toggleReadMore} className="read-or-hide">
+          {isReadMore ? READ_MORE : READ_LESS}
+        </span>
+      )}
+    </>
   );
 };
 
