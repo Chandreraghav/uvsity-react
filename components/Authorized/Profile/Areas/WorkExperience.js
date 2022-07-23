8e04ea0cbe530bbed86fba80811086ce7bb2fbed
ayanchax <42091907+ayanchax@users.noreply.github.com>
2022-07-23 19:40:32 +0530

    c

--- components/Authorized/Profile/Areas/WorkExperience.js
+++ components/Authorized/Profile/Areas/WorkExperience.js
@@ -1,18 +1,14 @@
-import React,{useState} from "react";
+import React from "react";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import { Grid } from "@mui/material";
 import NoData from "../../Shared/NoData";
-import { READ_LESS, READ_MORE, READ_MORE_MAX_LENGTH } from "../../../../constants/constants";
-import { parseMarkdownToHTML } from "../../../../utils/utility";
+import { READ_MORE_MAX_LENGTH } from "../../../../constants/constants";
 import { getMode, THEME_MODES } from "../../../../theme/ThemeProvider";
+import ReadMore from "../../../shared/ReadMore";
 function WorkExperience(props) {
-  const [isReadMore, setIsReadMore] = useState(true);
-  const toggleReadMore = () => {
-    setIsReadMore(!isReadMore);
-  };
   const experiences = props?.experiences;
   const getDurationOfexperience = (exp) => {
     let end = exp?.projResearchEndDateForDisplay;
@@ -26,7 +22,11 @@ function WorkExperience(props) {
     <React.Fragment>
       <CardContent>
         {exp?.projResearchStartDateForDisplay && (
-          <Typography sx={{ fontSize: 14 }} color={`${getMode()===THEME_MODES.DARK ? '':'text.secondary'}`} gutterBottom>
+          <Typography
+            sx={{ fontSize: 14 }}
+            color={`${getMode() === THEME_MODES.DARK ? "" : "text.secondary"}`}
+            gutterBottom
+          >
             {getDurationOfexperience(exp)}
           </Typography>
         )}
@@ -38,41 +38,49 @@ function WorkExperience(props) {
         )}
 
         {exp?.educationInstitution && (
-          <Typography sx={{ mb: 1.5 }} color={`${getMode()===THEME_MODES.DARK ? '':'text.secondary'}`}>
+          <Typography
+            sx={{ mb: 1.5 }}
+            color={`${getMode() === THEME_MODES.DARK ? "" : "text.secondary"}`}
+          >
             {exp?.educationInstitution} {exp?.campus ? `,${exp?.campus}` : ""}
           </Typography>
         )}
 
         {exp?.projectResearchDescription && (
           <>
-           
-            <Typography className={isReadMore && exp?.projectResearchDescription.length>READ_MORE_MAX_LENGTH?'line-clamp-3':''} variant="body2">
-            {parseMarkdownToHTML(exp?.projectResearchDescription)}
-           
-          </Typography>
-          <span onClick={toggleReadMore} className="read-or-hide">
-        {isReadMore && exp?.projectResearchDescription.length>READ_MORE_MAX_LENGTH ? READ_MORE : exp?.projectResearchDescription.length>READ_MORE_MAX_LENGTH?READ_LESS:''}
-      </span>
+            <ReadMore
+              parseHtml
+              initialReadLimit={READ_MORE_MAX_LENGTH}
+              color={`${
+                getMode() === THEME_MODES.DARK ? "" : "text.secondary"
+              }`}
+            >
+              {exp?.projectResearchDescription}
+            </ReadMore>
           </>
-        
         )}
       </CardContent>
     </React.Fragment>
   );
   return experiences && experiences.length > 0 ? (
     <>
-      <Box sx={{ width: "100%",maxHeight:400, overflow: "auto" }}>
+      <Box sx={{ width: "100%", maxHeight: 400, overflow: "auto" }}>
         <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
           {experiences?.map((experience, index) => (
             <Grid key={index} item xs={12} lg={6} md={6} sm={12}>
-              <Card className='dark:bg-gray-950 bg-white-100 dark:text-gray-500 text-gray-700' variant="outlined">{card(experience)}</Card>
+              <Card
+                className="dark:bg-gray-950 bg-white-100 dark:text-gray-500 text-gray-700"
+                variant="outlined"
+              >
+                {card(experience)}
+              </Card>
             </Grid>
           ))}
         </Grid>
       </Box>
     </>
   ) : (
-    <NoData message='No project or work experience available.'/>
+    <NoData message="No project or work experience available." />
   );
 }
 
